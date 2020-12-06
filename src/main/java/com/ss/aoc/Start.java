89fb3e13eb828dc2464c0818325faee7e5fea931
Stefanel Stan <stefanel.stan@yahoo.com>
2020-12-06 10:46:30 +0000

    Advent of Code 2020 day 06

--- src/main/java/com/ss/aoc/Start.java
+++ src/main/java/com/ss/aoc/Start.java
@@ -4,7 +4,8 @@ public class Start {
     public static void main(String[] args) {
         Start start = new Start();
 
-        start.day05();
+//        start.day05();
+        start.day06();
     }
 
     private void day05() {
@@ -15,5 +16,11 @@ public class Start {
         System.out.println("820 == " +day05.getHighestSeatId("BBFFBBFRLL"));
         System.out.println("MaxId == " +day05.getHighestSeatId());
         System.out.println("Missing Seat == " +day05.getMissingSeat());
+    }
+
+    private void day06() {
+        Day06 day06 = new Day06();
+        System.out.println(day06.sumOfYesAnswers());
+        System.out.println(day06.sumOfGroupYesAnwers());
     }
 }
