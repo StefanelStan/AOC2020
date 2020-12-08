dc903e01ebb39a433f52e22baab5bc67b6024023
Stefanel Stan <stefanel.stan@yahoo.com>
2020-12-08 20:19:40 +0000

    Advent of Code Day 08

--- src/main/java/com/ss/aoc/Start.java
+++ src/main/java/com/ss/aoc/Start.java
@@ -1,14 +1,13 @@
 package com.ss.aoc;
 
-import java.util.Arrays;
-
 public class Start {
     public static void main(String[] args) {
         Start start = new Start();
 
 //        start.day05();
 //        start.day06();
-        start.day07();
+//        start.day07();
+        start.day08();
     }
 
     private void day05() {
@@ -16,9 +15,9 @@ public class Start {
 
         System.out.println("357 == " + day05.getHighestSeatId("FBFBBFFRLR"));
         System.out.println("119 == " + day05.getHighestSeatId("FFFBBBFRRR"));
-        System.out.println("820 == " +day05.getHighestSeatId("BBFFBBFRLL"));
-        System.out.println("MaxId == " +day05.getHighestSeatId());
-        System.out.println("Missing Seat == " +day05.getMissingSeat());
+        System.out.println("820 == " + day05.getHighestSeatId("BBFFBBFRLL"));
+        System.out.println("MaxId == " + day05.getHighestSeatId());
+        System.out.println("Missing Seat == " + day05.getMissingSeat());
     }
 
     private void day06() {
@@ -28,19 +27,13 @@ public class Start {
     }
 
     private void day07() {
-//        String rule = "dotted gold bags contain 4 striped violet bags.";
-        String rule2 = "drab orange bags contain 3 wavy violet bags, 1 dark fuchsia bag, 2 mirrored green bags.";
-//
-//        System.out.println(Arrays.toString(rule.substring(rule.indexOf("contain ") + 8, rule.indexOf('.')).split(",")));
-//        String[] split = rule2.substring(rule2.indexOf("contain ") + 8, rule2.indexOf('.')).split(",");
-//        for (String chunk: split) {
-//            String noSpace = chunk.trim();
-//            String bagType = noSpace.substring(noSpace.indexOf(' ') + 1, noSpace.indexOf("bag") + 3);
-//            int quantity = Integer.parseInt(String.valueOf(noSpace.charAt(0)));
-//            System.out.println(bagType + quantity);
-//        }
         Day07 day07 = new Day07();
 //        System.out.println(day07.howManyBagsContain("shiny gold bag"));
         System.out.println(day07.howManyBagsInside("shiny gold bag"));
+    }
+
+    private void day08() {
+        Day08 day08 = new Day08();
+        System.out.println(day08.getValueOfAccumulator());
     }
 }
