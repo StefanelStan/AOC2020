58ca8de02384c4234b80049e64112ea8ec7da820
Stefanel Stan <stefanel.stan@yahoo.com>
2020-12-23 21:02:21 +0000

    Advent of Code Day 21 start

---
+++ src/main/java/com/ss/aoc/Day21.java
@@ -0,0 +1,19 @@
+package com.ss.aoc;
+
+public class Day21 extends AOCDay {
+
+    public Day21() {
+        this.dataFileName = "day21.txt";
+        this.testDataFilename = "day21test.txt";
+    }
+
+    @Override
+    long task1(boolean isTest) {
+        return 0;
+    }
+
+    @Override
+    long task2(boolean isTest) {
+        return 0;
+    }
+}
